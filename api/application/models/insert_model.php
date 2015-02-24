14a9b9c5ec376a2f15124f991cd4414ccbcf6696
Mike Hurwitz <mike@hrwtz.com>
2015-02-24 08:12:31 -0500

    Added func to get data from mint

--- api/application/models/insert_model.php
+++ api/application/models/insert_model.php
@@ -8,40 +8,13 @@ class Insert_model extends CI_Model {
     }
 
     function insert_categories($categories){
-        $categories_ready = array();
-        foreach ($categories as $category) {
-            $categories_ready[] = array(
-                'id' => $category->id,
-                'value' => $category->value,
-                'parent_id' => false
-            );
-            foreach ($category->children as $sub_category) {
-                $categories_ready[] = array(
-                    'id' => $sub_category->id,
-                    'value' => $sub_category->value,
-                    'parent_id' => $category->id
-                );
-            }
-        }
-        $this->db->insert_batch('categories', $categories_ready);
+        $this->db->empty_table('categories');
+        $this->db->insert_batch('categories', $categories);
     }
 
     function insert_transactions($transactions){
-        $transactions = $transactions->set[0]->data;
-        foreach ($transactions as $transaction) {
-            if ($transaction->isDuplicate || $transaction->isPending || $transaction->isTransfer)
-                continue;
-            $transactions_ready[] = array(
-                'account' => $transaction->account,
-                'category_id' => $transaction->categoryId,
-                'date' => date("Y-m-d H:i:s", strtotime($transaction->date)),
-                'amount' => str_replace('$', '', $transaction->amount),
-                'merchant' => $transaction->merchant,
-            );
-        }
-        $this->db->insert_batch('transactions', $transactions_ready);
+        $this->db->empty_table('transactions');
+        $this->db->insert_batch('transactions', $transactions);
     }
-    
-
 
 }
